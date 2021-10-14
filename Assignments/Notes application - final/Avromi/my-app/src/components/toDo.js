35581f1e7cfb5608b4230a9b83bc44aed3fb69a8
Avromi <avromirussell@gmail.com>
2021-10-14 14:17:18 +0300

    Milestone 4 complete with hooks

--- Assignments/Notes application - final/Avromi/my-app/src/components/toDo.js
+++ Assignments/Notes application - final/Avromi/my-app/src/components/toDo.js
@@ -1,6 +1,19 @@
 import dateFormat from "dateformat";
+import ReactModal from 'react-modal';
+import React, { useState } from "react"
+
+
+
 function ToDo(props){
-return <div style={{ 
+  const [currentState, setStateFunc] = useState(false)
+  function handleOpenModal () {
+    setStateFunc(!currentState)
+  }
+function handleCloseModal () {
+    setStateFunc(!currentState)
+  }
+
+return <div onClick={handleOpenModal} style={{ 
     padding: 18,
     width:'150px',
     border: '1px solid white',
@@ -14,6 +27,17 @@ return <div style={{
     {props.todo}
   <p style={{fontSize:'12px'}}>{dateFormat(props.time).toLocaleString()}</p>
   <button onClick={()=>props.deleteNote(props.index)}>Delete </button>
+  <ReactModal 
+           isOpen={currentState}
+           contentLabel="Minimal Modal Example">
+          <button onClick={handleCloseModal}>Close Modal</button>
+          <h3> {props.title}</h3>
+    {props.todo}
+  <p style={{fontSize:'12px'}}>{dateFormat(props.time).toLocaleString()}</p>
+  <button onClick={()=>props.deleteNote(props.index)}>Delete </button>
+        </ReactModal>
 </div>;
 }
+
+
 export default ToDo
