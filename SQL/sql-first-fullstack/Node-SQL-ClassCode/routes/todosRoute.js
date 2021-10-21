462582556648b834b5ef5a0a25da22fac6a6874b
Yonatan Salmon <yonatansalmon@itc.tech>
2021-10-21 21:00:25 +0300

    Class Code

---
+++ SQL/sql-first-fullstack/Node-SQL-ClassCode/routes/todosRoute.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const { query, getAllTodos, addTodo } = require("../data/todosDb.js");
+
+query(
+  `CREATE TABLE IF NOT EXISTS todos (
+    id      INT(200) AUTO_INCREMENT,
+    text    VARCHAR(255) NOT NULL,
+    created_date  DATE DEFAULT (CURRENT_DATE),
+    PRIMARY KEY(id))`
+)
+  .then(() => console.log("Table Created"))
+  .catch((err) => console.log(err));
+
+router.get("/", (req, res) => {
+  const allTodos = getAllTodos();
+  res.send(allTodos);
+});
+
+router.post("/", (req, res) => {
+  const { text } = req.body;
+  const addedTodo = addTodo(text);
+  console.log(addedTodo);
+  res.send("Todo Added");
+});
+
+module.exports = router;
